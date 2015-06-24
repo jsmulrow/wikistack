c964be35f1eb056544cd3a352beed5bd2585bc61
Jack Mulrow <jackmulrow@Jacks-MacBook-Pro.local>
2015-06-24 16:30:48 -0400

    Added edit and delete buttons

--- routes/pages.js
+++ routes/pages.js
@@ -14,7 +14,6 @@ module.exports = function(express) {
 
 	router.get("/:page/similar", function(req, res, next) {
 		Page.findOne({url_name: req.params.page}, function(err, page) {
-			
 			Page.find({
 				tags: {$in: page.tags},
 				url_name: {$ne: page.url_name}
@@ -22,7 +21,34 @@ module.exports = function(express) {
 				res.render("tags", { title: "Similar pages to " + page.title, pages: data });
 			});
 		});
-		
+	});
+
+	router.get("/:page/edit", function(req, res, next) {
+		Page.findOne({url_name: req.params.page}, function(err, page) {
+			res.render("edit", {title: "Edit: " + page.title, content: page.content, url_name: page.url_name, tags: page.tags.join(', ')});
+		});
+	});
+
+	router.post("/:page/edit/submit", function(req, res, next) {
+		var url_name = req.params.page;
+		Page.update(
+			{url_name: url_name},
+			{$set:
+				{
+					content: req.body.page_content,
+					tags: req.body.page_tags.split(/,\s*/)
+				}
+			}, function() {
+				res.redirect('/wiki/' + url_name);
+			}
+		);
+	});
+
+	router.get("/:page/delete", function(req, res, next) {
+		var page = req.params.page;
+		Page.remove({url_name: page}, function() {
+			res.redirect('/');
+		});
 	});
 
 	return router;
