988a62b4401aa95a4a59c26433dd49e5ea821121
Theo Lipeles <Theo@Lipeles.com>
2015-06-24 15:29:42 -0400

    Finished assignment

--- routes/pages.js
+++ routes/pages.js
@@ -7,7 +7,20 @@ module.exports = function(express) {
 		var page = req.params.page;
 
 		Page.findOne({url_name: page}, function(err, data) {
-			res.render("show", {title: data.title, content: data.content, tags: data.tags});
+			res.render("show", {title: data.title, content: data.content, tags: data.tags, url_name: data.url_name});
+		});
+		
+	});
+
+	router.get("/:page/similar", function(req, res, next) {
+		Page.findOne({url_name: req.params.page}, function(err, page) {
+			
+			Page.find({
+				tags: {$in: page.tags},
+				url_name: {$ne: page.url_name}
+			}, function(err, data) {
+				res.render("tags", { title: "Similar pages to " + page.title, pages: data });
+			});
 		});
 		
 	});
